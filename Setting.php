ed704ee08031e2b0297508c57212873ba609b6e6
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-02-06 10:39:13 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -2,7 +2,6 @@
 
 $versi = "1.0";
 $server = "online";
-$info = "tidak ada pembaruaan !";
 $bot = "webminer";
 
 $capthca = "${kuning}
@@ -18,7 +17,6 @@ ${putih}[created_by]${biru} Waryono${putih} [channel_by]${biru} Kang Repot
 ${putih}[support_by]${biru} Sandi Pratama${putih} [channel_by]${biru} Sandi X-Dark
 ${putih}[-] forum diskusi ${ijo}https://t.me/belajarnuyul
 ${ijo}                  https://t.me/kangrepot
-${kuning}[-] info${putih} ${info}
 ${merah}- - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
 $pesan="${merah}[!] warning !!
 ${kuning}[-] ini adalah progam ilegal
