d487b89d5b0a783f03300889752eac4b23b424ab
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-02-06 10:14:14 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,6 +1,8 @@
 <?php
 
 $versi = "1.0";
+$server = "online";
+$info = "tidak ada pembaruaan !";
 
 $capthca = "${kuning}
  ██████╗ █████╗ ██████╗ ████████╗
@@ -10,13 +12,14 @@ $capthca = "${kuning}
 ╚██████╗██║  ██║██║        ██║Doge
  ╚═════╝╚═╝  ╚═╝╚═╝        ╚═╝
 ${putih}[-] version${ijo} ${versi}${putih} (${kuning}Webminer${putih})
+${putih}[-] server${ijo} ${server}
 ${putih}[created_by]${biru} Waryono${putih} [channel_by]${biru} Kang Repot
 ${putih}[support_by]${biru} Sandi Pratama${putih} [channel_by]${biru} Sandi X-Dark
 ${putih}[-] forum diskusi ${ijo}https://t.me/belajarnuyul
 ${ijo}                  https://t.me/kangrepot
+${putih}[?] info${biru} ${info}
 ${merah}- - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
-$server="${putih}[-] server ${ijo}online \n";
-$pesan="[!] warning !!
+$pesan="${merah}[!] warning !!
 ${kuning}[-] ini adalah progam ilegal
 [-] resiko sepenuhnya di tanggung pengguna
 
