aac7c3bc0f0d76758bfac8301cb4aa65a07e63d4
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-02-06 10:19:33 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -3,6 +3,7 @@
 $versi = "1.0";
 $server = "online";
 $info = "tidak ada pembaruaan !";
+$bot = "webminer";
 
 $capthca = "${kuning}
  ██████╗ █████╗ ██████╗ ████████╗
@@ -11,17 +12,17 @@ $capthca = "${kuning}
 ██║     ██╔══██║██╔═══╝    ██║4
 ╚██████╗██║  ██║██║        ██║Doge
  ╚═════╝╚═╝  ╚═╝╚═╝        ╚═╝
-${putih}[-] version${ijo} ${versi}${putih} (${kuning}Webminer${putih})
+${putih}[-] version${ijo} ${versi}${putih} (${kuning}${bot}${putih})
 ${putih}[-] server${ijo} ${server}
 ${putih}[created_by]${biru} Waryono${putih} [channel_by]${biru} Kang Repot
 ${putih}[support_by]${biru} Sandi Pratama${putih} [channel_by]${biru} Sandi X-Dark
 ${putih}[-] forum diskusi ${ijo}https://t.me/belajarnuyul
 ${ijo}                  https://t.me/kangrepot
-${putih}[?] info${biru} ${info}
+${kuning}[-] info${putih} ${info}
 ${merah}- - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
 $pesan="${merah}[!] warning !!
 ${kuning}[-] ini adalah progam ilegal
-[-] resiko sepenuhnya di tanggung pengguna
+[-] resiko sepenuhnya di tanggung pengguna${putih}
 
 ";
 
