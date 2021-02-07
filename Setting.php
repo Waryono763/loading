3b7ea7c2e559e653acbd6e19adc7be9034eb361b
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-02-07 15:25:37 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,7 +1,7 @@
 <?php
 
 $versi = "1.0";
-$server = "online 👍";
+$server = "online";
 $bot = "webminer";
 
 $capthca = "${kuning}
@@ -23,5 +23,28 @@ ${kuning}[-] ini adalah progam ilegal
 [-] resiko sepenuhnya di tanggung pengguna${putih}
 
 ";
-
 $nuyul=true;
+
+$bch_versi  = "1.0";
+$bch_server = "online";
+$bch_info   = "claim faucet";
+
+$bchjolly = "
+${merah}   ___  _______ __     __     ____
+${putih}  / _ )/ ___/ // / __ / /__  / / /_ __
+${merah} / _  / /__/ _  / / // / _ \/ / / // /
+${putih}/____/\___/_//_/  \___/\___/_/_/\_, /
+                               /___/
+${putih}[-] version ${ijo}${bch_versi}${putih} (${kuning}${bch_info}${putih})
+${putih}[-] server ${ijo}${bch_server}
+${putih}[created_by]${biru} Waryono${putih} [channel_by]${biru} Kang Repot
+${putih}[support_by]${biru} Sandi Pratama${putih} [channel_by]${biru} Sandi X-Dark
+${putih}[-] forum diskusi${ijo} https://t.me/belajarnuyul
+${ijo}                  https://t.me/kangrepot
+${merah}- - - - - - - - - - - - - - - - - - - - - - - - - - -
+${merah}[!] warning !!
+${kuning}[-] ini adalah progam ilegal
+${kuning}[-] resiko sepenuhnya di tanggung pengguna
+
+";
+$bot=true;
