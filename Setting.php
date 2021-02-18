25898b7d9b1e8f488835b62c8b6b53e2b88ee33d
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-02-18 11:57:08 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,5 +1,22 @@
 <?php
 
+$ip_head = array(
+	"Host: v4.myipstack.com",
+	"Connection: keep-alive",
+	"User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36",
+	"Origin: https://hide.me",
+	"Referer: https://hide.me/",
+	"Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
+	);
+
+$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://v4.myipstack.com/");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $ip_head);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$ip_address = curl_exec($ch);
+
 $versi = "1.0";
 $server = "updating";
 $bot = "webminer";
@@ -173,7 +190,8 @@ $buzz_versi1  = "2.0";
 $buzz_server1 = "online";
 $bot_info1   = "bonus / news / video";
 
-$break_banner = "${putih}[-] script ->${ijo} [buzzbreak]
+$break_banner = "${putih} ip${ijo} ${ip_address}
+${putih}[-] script ->${ijo} [buzzbreak]
 ${putih}[-] version${ijo} $buzz_versi1${putih} [${kuning}$bot_info1${putih}]
 ${putih}[-] server bot ${ijo}${buzz_server1}
 ${putih}[-] Created by${biru} Waryono${putih} &&${biru} Sandi Pratama
