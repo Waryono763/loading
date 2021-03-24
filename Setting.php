28a9efae8788f0f171279ae197633b40d543038c
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-03-24 09:19:13 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -21,6 +21,23 @@ $ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 $not_found = curl_exec($ch);
 
+$bcn_versi = "1.0";
+$bcn_server = "online";
+
+$BCN = "${kuning}>${putih} Copyright © ${biru}Waryono 2021
+${kuning}>${putih} ip ${ijo}${not_found}
+${kuning}>${putih} server ${ijo}${bcn_server}
+${kuning}>${putih} versi ${ijo}${bcn_versi}${putih} (${ijo}gas legit ngab${putih})
+${kuning}>${putih} created by ${biru}Waryono
+
+${putih}         don't forget to subscribe                                              ${putih}                (${biru}Waryono Okw${putih})
+${merah}> warning !!
+${kuning}> this is an illegal program
+${kuning}> the risk is entirely borne by the user
+
+";
+$hog=true;
+
 $trx_versi = "1.0";
 $trx_server = "online";
 
@@ -29,7 +46,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${trx_server}
 ${kuning}>${putih} versi ${ijo}${trx_versi}${putih} (${ijo}tronrex.online${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -48,7 +64,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${cp_server}
 ${kuning}>${putih} versi ${ijo}${cp_versi}${putih} (${ijo}clipclaps${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -67,7 +82,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${ex_server}
 ${kuning}>${putih} versi ${ijo}${ex_versi}${putih} (${ijo}tinggal pake gak usah banyak bct${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -86,7 +100,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${lite_server}
 ${kuning}>${putih} versi ${ijo}${lite_versi}${putih} (${ijo}free litecoin${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -105,7 +118,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${faucet_server}
 ${kuning}>${putih} versi ${ijo}${faucet_versi}${putih} (${ijo}faucet-dogecoin.xyz${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -125,7 +137,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${beat_server}
 ${kuning}>${putih} versi ${ijo}${beat_versi}${putih} (${ijo}buzzbeat${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -163,7 +174,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${view_server}
 ${kuning}>${putih} versi ${ijo}${view_versi}${putih} (${ijo}gaskeun masih legit${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -182,7 +192,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${cash_server}
 ${kuning}>${putih} versi ${ijo}${cash_versi}${putih} (${ijo}cash buddy${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -201,7 +210,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${wheel_server}
 ${kuning}>${putih} versi ${ijo}${wheel_versi}${putih} (${ijo}Bitcoin Cash Wheel${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -239,7 +247,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${buzz_server}
 ${kuning}>${putih} versi ${ijo}${buzz_versi}${putih} (${ijo}buzzbreak${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
@@ -314,7 +321,6 @@ ${kuning}>${putih} ip ${ijo}${not_found}
 ${kuning}>${putih} server ${ijo}${free_server}
 ${kuning}>${putih} versi ${ijo}${free_versi}${putih} (${ijo}ndak bisa bhs inggris${putih})
 ${kuning}>${putih} created by ${biru}Waryono
-${kuning}>${ijo} paypal.me/WaryonoAj 😅
 
 ${putih}         don't forget to subscribe
 ${putih}               (${biru}Waryono Ok${putih})
